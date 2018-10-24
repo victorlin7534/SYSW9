30e35c387fe964ed06b4ff902fd1765fbab1aa5d
victorlin7534 <vlin5@stuy.edu>
2018-10-23 20:17:12 -0400

    3

--- file.c
+++ file.c
@@ -5,25 +5,21 @@
 #include <string.h>
 
 int main(){
-  int file = open("file.txt",O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+  int file = open("file.txt",O_CREAT | O_RDWR, S_IRWXG | S_IRWXO | S_IRWXU);
   printf("int value returned by open(): %d\n",file);
 
   char * sentence = "Whoever reads this likes potatoes and hot chocolate";
   int size = write(file,sentence,strlen(sentence));
   printf("int value returned by write(): %d\n",size);
 
-  char * data = (char*)calloc(size,1);
+  char data[size];
   size = read(file,data,size);
+  data[size] = '\0';
   printf("int value returned by read(): %d\n",size);
-
-  printf("string read:");
-  while(*data){
-    printf("%s",data);
-    data++;
-  }
-  printf("\n");
+  printf("string read: %s\n",data);
   
   file = close(file);
   printf("int value returned by close(): %d\n",file);
+
   return 0;
 }
